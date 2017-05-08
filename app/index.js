9c2764bd0e5bce410791a3767d010614e2dd6143
Evan Engels <evan.engels@gmail.com>
2017-05-08 15:29:58 -0500

    media queries and things

--- app/index.js
+++ app/index.js
@@ -1,7 +1,34 @@
 import 'whatwg-fetch';
 import Headroom from 'headroom.js';
+// import $ from 'jquery';
+// import waypoints from '../../package.json/waypoints/lib/noframework.waypoints';
 
 const element = document.querySelector('nav');
 const headroom = new Headroom(element);
 
 headroom.init();
+
+// class revealOnScroll {
+//   constructor() {
+//     this.itemsToReveal = $('.grid-profile');
+//     this.hideInitially();
+//     this.createWaypoints();
+//   }
+//
+//   hideInitially() {
+//     this.itemsToReveal.addClass('reveal-item');
+//   }
+//
+//   createWaypoints() {
+//     this.itemsToReveal.each(() => {
+//       const currentItem = this;
+//       new Waypoint({
+//         element: currentItem,
+//         handler() {
+//           $(currentItem).addClass('reveal-item--is-visible');
+//         },
+//         offset: '85%'
+//       });
+//     });
+//   }
+// }
